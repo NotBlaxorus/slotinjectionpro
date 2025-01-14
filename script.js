895360f74d14a632c9f148cc543eec7c8a4e2d02
NotBlaxorus <162192989+NotBlaxorus@users.noreply.github.com>
2025-01-14 13:37:43 -0800

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-// If you don’t need any JavaScript, you can omit this file.
-// Here's a simple script to auto-update the current year in the footer.
-
-const yearSpan = document.getElementById("year");
-const currentYear = new Date().getFullYear();
-yearSpan.textContent = currentYear;
